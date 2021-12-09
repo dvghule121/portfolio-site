2fefbe23ba2a05a57707a1c1340eeb28bb0a5a24
Dyno <dvghule121@gmail.com>
2021-12-10 00:26:24 +0530

    full-demo

--- new-index.js
+++ new-index.js
@@ -1,39 +1,40 @@
 
-    function cards() {
-        var car;
-        fetch("projects.json")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
+function cards() {
+    var car;
+    fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
 
-                var list = data["items"]
+            var list = data["items"]
 
 
 
-                const container = document.getElementById('ca');
-                list.forEach(function (item, index) {
-                    const card = `<div class="card-view">
+            const container = document.getElementById('ca');
+            list.forEach(function (item, index) {
+                const card = `<div class="card-view">
                             <div class="rounded-img"><img src="`+ item["img"] + `" alt="" height="400px" width="350px"> </div>
                             <div class="pr-name">
-                            <h1>`+ item["name"] + `</h1>
+                            <a href="`+item["link"]+`"><h1>`+ item["name"] + `</h1></a>
                             <p>`+ item["desc"] + `</p>
+                            
+                            
                             </div>
                             </div> `
 
-                    container.innerHTML += card
+                container.innerHTML += card
 
 
-                });
+            });
 
-            })
-    }
+        })
+}
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    function openImg(){
-        const class_c = document.getElementsByClassName("rounded-img-btn")
-        
-    }
+function openImg() {
+    const class_c = document.getElementsByClassName("rounded-img-btn")
 
+}
